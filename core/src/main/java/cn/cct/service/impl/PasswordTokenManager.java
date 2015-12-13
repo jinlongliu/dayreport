541d95e71483fc330c9202f349184a565e0d1cb1
Jinlong Liu <liujl20150418@icloud.com>
2015-12-13 21:18:38 +0800

    First Add.

---
+++ core/src/main/java/cn/cct/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package cn.cct.service.impl;
+
+import cn.cct.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
