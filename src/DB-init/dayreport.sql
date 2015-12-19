77a504d553a21856b4b3855afc76deb126b3fbdd
Jinlong Liu <liujl20150418@icloud.com>
2015-12-19 18:05:52 +0800

    数据库初始化脚本

---
+++ src/DB-init/dayreport.sql
@@ -0,0 +1,104 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 192.168.50.50
+Source Server Version : 50546
+Source Host           : 192.168.50.50:3306
+Source Database       : dayreport
+
+Target Server Type    : MYSQL
+Target Server Version : 50546
+File Encoding         : 65001
+
+Date: 2015-12-19 18:01:54
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for app_user
+-- ----------------------------
+DROP TABLE IF EXISTS `app_user`;
+CREATE TABLE `app_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `account_expired` bit(1) NOT NULL,
+  `account_locked` bit(1) NOT NULL,
+  `address` varchar(150) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `country` varchar(100) DEFAULT NULL,
+  `postal_code` varchar(15) DEFAULT NULL,
+  `province` varchar(100) DEFAULT NULL,
+  `credentials_expired` bit(1) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `account_enabled` bit(1) DEFAULT NULL,
+  `first_name` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `last_name` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `password_hint` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(255) DEFAULT NULL,
+  `username` varchar(50) NOT NULL,
+  `version` int(11) DEFAULT NULL,
+  `website` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_1j9d9a06i600gd43uu3km82jw` (`email`),
+  UNIQUE KEY `UK_3k4cplvh82srueuttfkwnylq0` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of app_user
+-- ----------------------------
+INSERT INTO `app_user` VALUES ('-1', '\0', '\0', '', 'Denver', 'US', '80210', 'CO', '\0', 'educloud@huaxunchina.cn', '', '系统', '管理员', '$2a$10$bH/ssqW8OhkTlIso9/yakubYODUOmh.6m5HEJvcBq3t3VdBh7ebqO', 'Not a female kitty.', '', 'admin', '4', 'http://raibledesigns.com');
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `description` varchar(64) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES ('-2', 'Default role for all Users', 'ROLE_USER');
+INSERT INTO `role` VALUES ('-1', 'Administrator role (can edit Users)', 'ROLE_ADMIN');
+
+-- ----------------------------
+-- Table structure for user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role` (
+  `user_id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `FK_it77eq964jhfqtu54081ebtio` (`role_id`),
+  CONSTRAINT `FK_apcc8lxk2xnug8377fatvbn04` FOREIGN KEY (`user_id`) REFERENCES `app_user` (`id`),
+  CONSTRAINT `FK_it77eq964jhfqtu54081ebtio` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of user_role
+-- ----------------------------
+INSERT INTO `user_role` VALUES ('-1', '-1');
+
+-- ----------------------------
+-- Table structure for workday_report
+-- ----------------------------
+DROP TABLE IF EXISTS `workday_report`;
+CREATE TABLE `workday_report` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `last_update_time` datetime DEFAULT NULL,
+  `today_report` varchar(255) DEFAULT NULL,
+  `tomrrow_plan` varchar(255) DEFAULT NULL,
+  `write_time` datetime DEFAULT NULL,
+  `user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_qa8mca4sdf3mw2rm1a2aqsexq` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of workday_report
+-- ----------------------------
